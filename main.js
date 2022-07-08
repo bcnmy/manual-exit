bea3d8d4a07479e88bef2166b8fc3e02910671bd
Asheer Rizvi <asheer.vil@gmail.com>
2022-07-08 12:42:03 +0530

    feat: manual exit form

---
+++ main.js
@@ -0,0 +1,43 @@
+import "./style.css";
+
+import "./style.css";
+
+const app = document.querySelector("#app");
+const manualExitForm = app.querySelector("#form");
+const message = app.querySelector(".message");
+
+async function handleFormSubmit(e) {
+  e.preventDefault();
+
+  const inputs = e.target.elements;
+  const depositHash = inputs["depositHash"].value;
+  const fromChainId = inputs["fromChainId"].valueAsNumber;
+
+  const requestData = {
+    depositHash,
+    fromChainId,
+  };
+
+  const res = await window.fetch(
+    "https://hyphen-v2-api.biconomy.io/api/v1/insta-exit/execute",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    }
+  );
+
+  if (res.ok) {
+    const data = await res.json();
+    const exitHash = data.exitHash;
+    message.textContent = `Exit hash: ${exitHash}`;
+    message.className = "message success";
+  } else {
+    message.textContent = `Error: ${res.status}`;
+    message.className = "message error";
+  }
+}
+
+manualExitForm?.addEventListener("submit", handleFormSubmit);
